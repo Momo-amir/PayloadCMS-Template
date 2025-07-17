23bb4cb079f128c03b0e3e9f9a66f7406e6ebdad
Momo Amer <maea@kollab.dk>
2025-07-17 16:50:54 +0200

    Cleans up layout approach in favour of tailwindcss container

--- src/website/blocks/TwoBlock/Component.tsx
+++ src/website/blocks/TwoBlock/Component.tsx
@@ -5,7 +5,7 @@ export const TwoBlock: React.FC<{
   left?: TwoBlockField[]
   right?: TwoBlockField[]
 }> = ({ left = [], right = [] }) => (
-  <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 items-center col-span-12">
+  <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 items-center container">
     {[left, right].map((column, colIdx) => (
       <div key={colIdx}>
         {column.map((field, i) => renderChildField(field, `${colIdx}-${i}`, true))}
