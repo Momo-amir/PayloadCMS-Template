676b0a421376c5bc6b05d30dd5a7fcf191d67763
Momo Amer <maea@kollab.dk>
2025-08-27 12:30:17 +0200

    Fixes asset name change for admin login

--- src/cms/components/Graphics/Logo.tsx
+++ src/cms/components/Graphics/Logo.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
 
 export default function Logo() {
-  return <Image src="/assets/logo.svg" alt="Logo" width={200} height={200} />
+  return <Image src="/assets/logo-on-light.svg" alt="Logo" width={200} height={200} />
 }
