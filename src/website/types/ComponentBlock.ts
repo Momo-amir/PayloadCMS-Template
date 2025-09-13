1a879ee83169e2e845fef7bd349efb80dee8946f
Lucasion <lucasion@hotmail.com>
2025-09-13 03:47:29 +0200

    Minor comments for clarification

--- src/website/types/ComponentBlock.ts
+++ src/website/types/ComponentBlock.ts
@@ -2,6 +2,12 @@ import { Block } from 'payload';
 import { FC } from 'react';
 
 export interface ComponentBlock extends Block {
+  /**
+   * Determines if this block is selectable on a Page collection.
+   */
   showOnPage?: boolean;
+  /**
+   * The functional React component to render for this block.
+   */
   component: FC<any>;
 };
