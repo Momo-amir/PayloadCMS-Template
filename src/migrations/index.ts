78701294884b943496f5c27f5f23002a8ed3269d
Momo Amer <maea@kollab.dk>
2025-10-05 22:55:28 +0200

    Creates simple docker production flow with migrations, updates route.ts
    and cleans up script files from unused commands

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251005_184831 from './20251005_184831';
+
+export const migrations = [
+  {
+    up: migration_20251005_184831.up,
+    down: migration_20251005_184831.down,
+    name: '20251005_184831'
+  },
+];
