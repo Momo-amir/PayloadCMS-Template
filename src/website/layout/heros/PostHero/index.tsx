8be55d043607366414772ac1ea23106d8b0059bd
Momo Amer <maea@kollab.dk>
2025-10-07 21:05:51 +0200

    removes force dark on post

--- src/website/layout/heros/PostHero/index.tsx
+++ src/website/layout/heros/PostHero/index.tsx
@@ -5,7 +5,6 @@ import type { Post } from '@/payload-types'
 
 import { Media } from '@/website/components/Media'
 import { formatAuthors } from '@/cms/utilities/formatAuthors'
-import { useHeaderThemeOverride } from '@/providers/Theme/LocalTheme/LocalTheme'
 
 export const PostHero: React.FC<{
   post: Post
@@ -14,9 +13,6 @@ export const PostHero: React.FC<{
 
   const hasAuthors =
     populatedAuthors && populatedAuthors.length > 0 && formatAuthors(populatedAuthors) !== ''
-
-  useHeaderThemeOverride('dark')
-
   return (
     <div className="relative -mt-[10.4rem] flex items-end">
       <div className="container z-10 relative lg:grid lg:grid-cols-[1fr_48rem_1fr] text-white pb-8">
