be6de74ffd29f4963884b8e9a62ca7d5a92d8d34
Momo Amer <maea@kollab.dk>
2025-12-23 17:28:34 +0100

    Drops db for prod one time

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+node --import ./scss-loader.mjs ./node_modules/.bin/payload migrate:fresh --config src/payload.config.ts || true  
 node --import ./scss-loader.mjs ./node_modules/.bin/payload migrate --config src/payload.config.ts
 exec node server.js
