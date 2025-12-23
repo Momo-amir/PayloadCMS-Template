4e65a211e4bdd0077ee8ab46993a9336a93be74a
Momo Amer <maea@kollab.dk>
2025-12-23 18:59:10 +0100

    a

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-node --import ./scss-loader.mjs ./node_modules/.bin/payload migrate --config src/payload.config.ts
+node --import ./scss-loader.mjs ./node_modules/.bin/payload migrate --config src/payload.config.ts || true
 exec node server.js
+
