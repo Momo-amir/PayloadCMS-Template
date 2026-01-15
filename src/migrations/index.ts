417d6164ebbe0c8aecb729958135130846c76676
Momo Amer <maea@kollab.dk>
2026-01-15 17:23:08 +0100

    Adds migrations for new changes

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20251223_184311 from './20251223_184311';
 import * as migration_20260114_175151 from './20260114_175151';
+import * as migration_20260115_162244 from './20260115_162244';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260114_175151.up,
     down: migration_20260114_175151.down,
-    name: '20260114_175151'
+    name: '20260114_175151',
+  },
+  {
+    up: migration_20260115_162244.up,
+    down: migration_20260115_162244.down,
+    name: '20260115_162244'
   },
 ];
