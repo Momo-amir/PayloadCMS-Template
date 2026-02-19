3846572aff1188e9816ceb759e8f19b371258a35
Lucas <lun@kollab.dk>
2026-02-19 19:55:03 +0100

    Reduced speed of accordion

--- src/website/blocks/Accordion/Component.tsx
+++ src/website/blocks/Accordion/Component.tsx
@@ -52,7 +52,7 @@ export const Accordion: React.FC<AccordionProps> = ({ title, content, grouped, l
           <div
             className={cn(
               'overflow-hidden transition-all duration-200',
-              actualOpen ? 'max-h-[2000px] opacity-100' : 'max-h-0 opacity-0',
+              actualOpen ? 'max-h-[1000px] opacity-100' : 'max-h-0 opacity-0',
             )}
           >
             <div className="pb-4">
