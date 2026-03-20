4291f29f921ff0b813631326c9414f1da8950d59
Momo Amir Kruse Amer <maea@kollab.dk>
2026-03-20 11:49:14 +0100

    makes sure all locales are revalidated on change

--- src/website/layout/Footer/hooks/revalidateFooter.ts
+++ src/website/layout/Footer/hooks/revalidateFooter.ts
@@ -1,12 +1,16 @@
 import type { GlobalAfterChangeHook } from 'payload'
 
 import { revalidateTag } from 'next/cache'
+import localization from '@/i18n/localization'
 
 export const revalidateFooter: GlobalAfterChangeHook = ({ doc, req: { payload, context } }) => {
   if (!context.disableRevalidate) {
     payload.logger.info(`Revalidating footer`)
 
     revalidateTag('global_footer', 'max')
+    for (const locale of localization.locales) {
+      revalidateTag(`global_footer_${locale.code}`, 'max')
+    }
   }
 
   return doc
--- src/website/layout/Header/hooks/revalidateHeader.ts
+++ src/website/layout/Header/hooks/revalidateHeader.ts
@@ -1,12 +1,16 @@
 import type { GlobalAfterChangeHook } from 'payload'
 
 import { revalidateTag } from 'next/cache'
+import localization from '@/i18n/localization'
 
 export const revalidateHeader: GlobalAfterChangeHook = ({ doc, req: { payload, context } }) => {
   if (!context.disableRevalidate) {
     payload.logger.info(`Revalidating header`)
 
     revalidateTag('global_header', 'max')
+    for (const locale of localization.locales) {
+      revalidateTag(`global_header_${locale.code}`, 'max')
+    }
   }
 
   return doc
